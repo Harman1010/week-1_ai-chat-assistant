38bdbeaa4be3e340f1c7ce04d80b0ff398b29764
Harman1010 <hsabharwal5@gmail.com>
2026-07-10 16:28:31 +0530

    Completed the project

--- core/routes.py
+++ core/routes.py
@@ -6,6 +6,12 @@ from core.schemas import UserRequest,AIResponse
 
 router = APIRouter()
 
+@router.get("/")
+def status():
+    return {
+        "status" : "running"
+    }
+
 @router.post("/chat")
 def request(request : UserRequest):
     response = ask_ai(request.message)
